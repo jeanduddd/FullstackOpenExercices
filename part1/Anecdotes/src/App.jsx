42b6467de14c8a67bc9d11d97bb0d145834a857d
jeanduddd <116556809+jeanduddd@users.noreply.github.com>
2026-09-06 17:43:22 +0200

    feat : implement ex1.13

--- part1/Anecdotes/src/App.jsx
+++ part1/Anecdotes/src/App.jsx
@@ -13,13 +13,16 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
-
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+  
   return (
     <>
     <div>
       {anecdotes[selected]}
     </div>
-    <button onClick={() => setSelected(Math.floor(Math.random()*anecdotes.length))}>Random Anecdote</button>
+    <p>has {votes[selected]} votes</p>
+    <button onClick={() => {const copy = [ ...votes ]; copy[selected]+=1; setVotes(copy)}}>vote</button>
+    <button onClick={() => setSelected(Math.floor(Math.random()*anecdotes.length))}>Next Anecdote</button>
     </>
   )
 }
