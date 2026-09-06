706f6091db1d23e2242b4502b512333cd9ebbf7f
jeanduddd <116556809+jeanduddd@users.noreply.github.com>
2026-09-06 16:53:29 +0200

    feat : implement ex1.7

--- part1/Unicafe/src/App.jsx
+++ part1/Unicafe/src/App.jsx
@@ -18,6 +18,10 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const total = good+neutral+bad
+  const average = total === 0 ? 0 : (good-bad)/total
+  const positive = total === 0 ? 0 : (good/total)*100
+
   return (
     <div>
       <h2>give feedback</h2>
@@ -30,6 +34,9 @@ const App = () => {
       <p style={{margin: 0}}>good {good}</p>
       <p style={{margin: 0}}>neutral {neutral}</p>
       <p style={{margin: 0}}>bad {bad}</p>
+      <p style={{margin: 0}}>total {total}</p>
+      <p style={{margin: 0}}>average {average}</p>
+      <p style={{margin: 0}}>positive {positive} %</p>
     </div>
   )
 }
