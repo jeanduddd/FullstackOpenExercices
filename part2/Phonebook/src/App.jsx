e6e4763134cad8cf8b1a93bfddffabe8be1267fd
jeanduddd <116556809+jeanduddd@users.noreply.github.com>
2026-09-07 21:23:13 +0200

    feat : implement ex2.6

---
+++ part2/Phonebook/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addName = () => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input
+          value={newName}
+          onChange={handleNoteChange}
+        />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+    </div>
+  )
+}
+
+export default App
