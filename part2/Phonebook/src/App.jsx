920b7044309fc12e30a42f98b2a630687df8af38
jeanduddd <116556809+jeanduddd@users.noreply.github.com>
2026-09-07 21:37:02 +0200

    feat : implement ex2.7

--- part2/Phonebook/src/App.jsx
+++ part2/Phonebook/src/App.jsx
@@ -11,13 +11,18 @@ const App = () => {
     setNewName(event.target.value)
   }
 
-  const addName = () => {
+  const addName = (event) => {
     event.preventDefault()
-    const nameObject = {
-      name: newName
+    if (persons.filter(person => person.name === newName).length === 0){
+      const nameObject = {
+        name: newName
+      }
+      setPersons(persons.concat(nameObject))
     }
-
-    setPersons(persons.concat(nameObject))
+    else{
+      alert(`${newName} is already added to phonebook`)
+      
+    }
     setNewName('')
   }
 
