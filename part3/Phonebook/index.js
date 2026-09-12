19aa6dbe099ac07c2a7be9973e95bd207ebab63c
jeanduddd <116556809+jeanduddd@users.noreply.github.com>
2026-09-12 23:35:52 +0200

    feat : implement ex3.4

--- part3/Phonebook/index.js
+++ part3/Phonebook/index.js
@@ -41,6 +41,13 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  phonebook = phonebook.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
 app.get('/info', (request, response) => {
   const date = new Date()
   const length = phonebook.length
